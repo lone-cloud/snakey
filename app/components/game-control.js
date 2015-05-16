62e575281763e3166b6cd2ca167ca0c95a1e98d7
Egor <hoboman313@gmail.com>
2015-05-15 19:07:32 -0700

    Adding game sound + prettifying

--- app/components/game-control.js
+++ app/components/game-control.js
@@ -1,14 +1,52 @@
-import Ember from 'ember';
+import Em from 'ember';
 
-export default Ember.Component.extend({
+export default Em.Component.extend({
+
+  actions: {
+    newGame: function(){
+      this.sendAction('newGameClick');
+    },
+
+    toggleSound: function(){
+      var soundOff = !this.get('soundOff');
+
+      this.set('soundOff', soundOff);
+
+      if(soundOff){
+        Em.$('#game-song').trigger('pause');
+      } else {
+        Em.$('#game-song').trigger('play');
+      }
+
+      if(typeof(Storage) !== "undefined"){
+        localStorage.setItem('snakey.soundOff', soundOff);
+      }
+    }
+  },
+
+  didInsertElement: function() {
+    if(typeof(Storage) !== "undefined") {
+      var soundOff = JSON.parse(localStorage.getItem('snakey.soundOff'));
+
+      // JSON.parse because localStorage only stores as String...weird
+      if(!soundOff){
+        this.set('soundOff', soundOff);
+        Em.$('#game-song').trigger('play');
+      }
+    }
+  },
 
   score: null,
 
   highScore: null,
 
-  actions: {
-    newGame: function(){
-      this.sendAction('newGameClick');
+  soundOff: true,
+
+  soundClass: function(){
+    if(this.get('soundOff')){
+      return 'sound-off';
+    } else {
+      return 'sound-on';
     }
-  }
+  }.property('soundOff')
 });
